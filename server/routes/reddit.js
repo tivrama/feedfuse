286eeeb9a76ef6332ff07eb4b9956b2ac72e2236
Mario Roaf-Esparza <marioroafesparza@gmail.com>
2016-06-24 16:24:20 -0700

    add Reddit service

--- server/routes/reddit.js
+++ server/routes/reddit.js
@@ -7,10 +7,13 @@ module.exports = function(app) {
   app.post('/api/reddit', function (req, res) {
     //get query from req
     var query = req.body.search;
+    // make the request of reddit
     request('https://json.reddit.com/search?q=' + query, function(error, response, body) {
       if (error) {
         console.log('Something went wrong with reddit', error);
+        res.send(error);
       } else {
+        //send off the results
         res.send(body);
       }
     });
