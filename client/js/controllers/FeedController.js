08d6d84b84054bb9c1b7a03c206dd4894f84a229
tivrama <tivrama@yahoo.com>
2016-06-25 11:44:58 -0700

    Changes to FeedController - Reddit now sends to sort

--- client/js/controllers/FeedController.js
+++ client/js/controllers/FeedController.js
@@ -23,71 +23,98 @@ angular.module('ff.controllers').controller('FeedController', function($scope, F
       //<END---------------------reddit testing---------------------------->
 
 
-      //<START-------------------CHANGE INSTAGRAM TO REDDIT---------------->
-      //<START-------------------CHANGE INSTAGRAM TO REDDIT---------------->
+// twitter promise
+     var twitterGet = Twitter.getData($scope.query).then(function(results) {
+       // If Twitter was authorized, store the returned results array
+       // If not, set it to undefined
 
-      Twitter.getData($scope.query).then(function(results) {
-        // If Twitter was authorized, store the returned results array
-        // If not, set it to undefined
+       if (!results.data) {
+         $scope.twitterData = undefined;
+       } else {
+         $scope.twitterData = results.data;
+       }
+     });
 
-        if (!results.data) {
-          $scope.twitterData = undefined;
-        } else {
-          $scope.twitterData = results.data;
-        }
-      }).then(function(){
-          // Get search results from reddit
-          Reddit.getData($scope.query).then(function(results) {
-            var data = results.data.data.children;
-            //store results in $scope for sort
-            //check if data.length is greater than 10
-            if (data.length > 10) {
-              data = data.slice(0,9);
-            }
-            $scope.redditData = data;
-          });
-        }).then(function(){
-          if ($scope.twitterData !== undefined && $scope.redditData !== undefined) {
-            // Both Twitter and reddit accounts were authorized
+     //reddit promise
+     var redditGet =  Reddit.getData($scope.query).then(function(results) {
+       var data = results.data.data.children;
+       //store results in $scope for sort
+       //check if data.length is greater than 10
+       if (data.length > 10) {
+         data = data.slice(0, 10);
+       }
+       $scope.redditData = data;
+       console.log($scope.redditData, 'redditData in presort');
+     });
 
-            // Check whether data for both actually exists
-            if ($scope.twitterData.length <= 0 && $scope.redditData.length <= 0) {
-              Feed.setDataExists(false); // Set flag for no data found alert
-              $state.go('home'); // Return state to home
-            } else {
-              $scope.sort($scope.twitterData, $scope.redditData); // Invoke sort function
-            }
-          } else if ($scope.twitterData !== undefined && $scope.redditData === undefined) {
-            // Only Twitter authorized
+     $q.all([twitterGet, redditGet]).then(function() {
+       $scope.sort($scope.twitterData, $scope.redditData);
+     });
+      // //<START-------------------CHANGE INSTAGRAM TO REDDIT---------------->
+      // //<START-------------------CHANGE INSTAGRAM TO REDDIT---------------->
 
-            // Check whether Twitter data actually exists
-            if ($scope.twitterData.length <= 0){
-              Feed.setDataExists(false); // Set flag for no data found alert
-              $state.go('home'); // Return state to home
-            } else {
-              $scope.sort($scope.twitterData, null); // Invoke sort function
-            }
-          } else if ($scope.redditData !== undefined && $scope.twitterData === undefined) {
-            // Only reddit authorized
+      // Twitter.getData($scope.query).then(function(results) {
+      //   // If Twitter was authorized, store the returned results array
+      //   // If not, set it to undefined
 
-            // Check whether reddit data actually exists
-            if ($scope.redditData.length <= 0){
-              Feed.setDataExists(false); // Set flag for no data found alert
-              $state.go('home'); // Return state to home
-            } else {
-              $scope.sort(null, $scope.redditData); // Invoke sort function
-            }
-          }
-        }).catch(function(err) {
-          // reddit catch()
-          console.error(err);
-        })
-      .catch(function(err) {
-        // Twitter catch()
-        console.error(err);
-      });
-      //<END-------------------CHANGE INSTAGRAM TO REDDIT---------------->
-      //<END-------------------CHANGE INSTAGRAM TO REDDIT---------------->
+      //   if (!results.data) {
+      //     $scope.twitterData = undefined;
+      //   } else {
+      //     $scope.twitterData = results.data;
+      //   }
+      // }).then(function(){
+      //     // Get search results from reddit
+      //     Reddit.getData($scope.query).then(function(results) {
+      //       var data = results.data.data.children;
+      //       //store results in $scope for sort
+      //       //check if data.length is greater than 10
+      //       if (data.length > 10) {
+      //         data = data.slice(0,9);
+      //       }
+      //       $scope.redditData = data;
+      //     });
+      //   }).then(function(){
+      //     if ($scope.twitterData !== undefined && $scope.redditData !== undefined) {
+      //       // Both Twitter and reddit accounts were authorized
+
+      //       // Check whether data for both actually exists
+      //       if ($scope.twitterData.length <= 0 && $scope.redditData.length <= 0) {
+      //         Feed.setDataExists(false); // Set flag for no data found alert
+      //         $state.go('home'); // Return state to home
+      //       } else {
+      //         $scope.sort($scope.twitterData, $scope.redditData); // Invoke sort function
+      //       }
+      //     } else if ($scope.twitterData !== undefined && $scope.redditData === undefined) {
+      //       // Only Twitter authorized
+
+      //       // Check whether Twitter data actually exists
+      //       if ($scope.twitterData.length <= 0){
+      //         Feed.setDataExists(false); // Set flag for no data found alert
+      //         $state.go('home'); // Return state to home
+      //       } else {
+      //         $scope.sort($scope.twitterData, null); // Invoke sort function
+      //       }
+      //     } else if ($scope.redditData !== undefined && $scope.twitterData === undefined) {
+      //       // Only reddit authorized
+
+      //       // Check whether reddit data actually exists
+      //       if ($scope.redditData.length <= 0){
+      //         Feed.setDataExists(false); // Set flag for no data found alert
+      //         $state.go('home'); // Return state to home
+      //       } else {
+      //         $scope.sort(null, $scope.redditData); // Invoke sort function
+      //       }
+      //     }
+      //   }).catch(function(err) {
+      //     // reddit catch()
+      //     console.error(err);
+      //   })
+      // .catch(function(err) {
+      //   // Twitter catch()
+      //   console.error(err);
+      // });
+      // //<END-------------------CHANGE INSTAGRAM TO REDDIT---------------->
+      // //<END-------------------CHANGE INSTAGRAM TO REDDIT---------------->
 
     }
   }, true);
@@ -97,7 +124,7 @@ angular.module('ff.controllers').controller('FeedController', function($scope, F
   $scope.sort = function(twitter, reddit) {
     // twitter = twitterData array
     // reddit = redditData array
-    console.log('reddit: ', reddit);
+    // console.log('reddit inside sort: ', reddit);
 
     $scope.unsorted = []; // Initialize unsorted array
     
@@ -123,10 +150,9 @@ angular.module('ff.controllers').controller('FeedController', function($scope, F
 //----------reddit----------------------------------------
     if (reddit !== null) {              // Checks to see if Reddit data was passed
       // Convert Reddit timestamp to be consistent with Twitter as epoch time
-      console.log('What is Reddit: ', reddit);
       
       reddit.forEach(function(val){
-        var time = val.created_utc; //EXAMPLE 1305208232.0
+        var time = val.data.created_utc; //EXAMPLE 1305208232.0
         val.created_at = parseInt(time) * 1000; // Convert Reddit created_at time to milliseconds
         val.source_network = 'reddit'; // Add flag for data source
         
